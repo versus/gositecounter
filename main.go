9bf87a93b2787a5d72921b6a0f27252ce537b743
Valentin <versus.ua@gmail.com>
2017-04-24 00:22:39 +0300

    move to worker pool

--- main.go
+++ main.go
@@ -3,38 +3,46 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
-	"sync"
 	"sync/atomic"
 	"time"
 )
 
-func main() {
-	var status200 uint64 = 0
-	var summ uint64 = 0
-	var wg sync.WaitGroup
-	messages := make(chan bool)
-	signals := make(chan bool)
-
-	go func() {
-		var counter uint64
-		counter = 0
-		for {
-			select {
-			case msg := <-messages:
-				if msg {
-					counter++
+func worker(id int, jobs <-chan string, signals <-chan bool, result chan<- bool) {
+	for {
+		select {
+		case site := <-jobs:
+			if len(site) > 1 {
+				fmt.Println("Worker: ", id, "Site: ", site)
+				resp, err := http.Get("http://" + site)
+				if (err == nil) && (resp.StatusCode == 200) {
+					fmt.Println("Worker: ", id, "Site: ", site, resp.StatusCode)
+					result <- true
+				} else {
+					log.Println(err)
 				}
-			case <-signals:
-				atomic.AddUint64(&status200, counter)
-				break
-			default:
-
 			}
+		case sig := <-signals:
+			if sig {
+				break
+			}
+		default:
 		}
-	}()
+	}
+}
+
+func main() {
+	var summ, status200 uint64 = 0, 0
+	signals := make(chan bool)
+	jobs := make(chan string, 10)
+	result := make(chan bool)
+
+	for w := 1; w <= 10; w++ {
+		go worker(w, jobs, signals, result)
+	}
 
 	file, err := os.Open("topmillion.txt")
 	defer file.Close()
@@ -43,19 +51,19 @@ func main() {
 	}
 	s := bufio.NewScanner(file)
 	for s.Scan() {
+
 		site := strings.Fields(s.Text())[1]
-		wg.Add(1)
 		summ++
-		go func(site string) {
-			defer wg.Done()
-			resp, err := http.Get("http://" + site)
-			if (err == nil) && (resp.StatusCode == 200) {
-				messages <- true
-			}
-		}(site)
+		jobs <- site
+
 	}
-	wg.Wait()
+	close(jobs)
+	for range result {
+		atomic.AddUint64(&status200, 1)
+	}
+
 	signals <- true
+	close(result)
 	time.Sleep(time.Second)
 	fmt.Println("summ: ", summ, "Status 200:", atomic.LoadUint64(&status200))
 }
